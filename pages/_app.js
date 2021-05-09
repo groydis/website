c2c35dbc75a2ebbd688b8f158a4736a6265df2d7
greyden <greydenscott@gmail.com>
2021-05-10 09:48:12 +1000

    About Page

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 // import "xp.css/dist/XP.css";
 import "xp.css/dist/98.css";
 import '../styles/globals.css'
+// import '../styles/about.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/about.js
+++ pages/about.js
@@ -1,60 +1,84 @@
-import {bgWrap, center} from '../styles/Home.module.css'
-import Image from 'next/image'
-import Link from 'next/link'
+import { abWrap, center } from "../styles/Home.module.css";
+// import '../styles/about.module.css'
+import Image from "next/image";
+// import Link from 'next/link'
+// import "nes.css/css/nes.min.css";
+import "hack";
 
 export default function Home() {
   return (
-    <div>
-      <div className={bgWrap}>
+    <div className={abWrap}>
+      {/* <div className={center}>
         <Image
-          alt="Majestic View"
-          src="/background.jpg"
-          layout="fill"
-          objectFit="cover"
-          quality={100}
+          alt="Awful retro gif that says resume."
+          src="/flamebg.jpg"
+          width={206}
+          height={44}
+          layout="responsive"
         />
-      </div>
+      </div> */}
       <div className={center}>
+        <h1>About Me</h1>
+        <p>
+          My name is Greyden and I am a software developer from Brisbane,
+          Australia.
+          <br />
+          This website was built as a homage to the era of internet I grew up
+          with.
+          <br />
+          Talking about yourself is boring, here is a bunch of stuff I could of
+          done had I organised my life better.
+        </p>
         <Image
-          alt="Classic Word out that reads - Greyden Scott"
-          src="/wordart-gs.png"
-          width={1797}
-          height={885}
+          alt="Awful retro gif that says resume."
+          src="/queen.png"
+          width={976}
+          height={549}
+          layout="responsive"
         />
+        <p>
+          This one time I really wanted to meet the queen, but not be dressed
+          for the occasion. In this instance I'm dressed up as Malakili the
+          Rancor Handler from star wars. This is a low effort cosplay. I am
+          patiently waiting my turn to shake her hand.
+        </p>
+        <Image
+          alt="Awful retro gif that says resume."
+          src="/obama.png"
+          width={900}
+          height={600}
+          layout="responsive"
+        />
+        <p>
+          I've always felt like I had the potential to influence global
+          politics, in this instance I had just helped Obama with global
+          policies and had casually talked about upcomming new olympic sport
+          categories.
+        </p>
+        <Image
+          alt="Awful retro gif that says resume."
+          src="/karate.png"
+          width={900}
+          height={600}
+          layout="responsive"
+        />
+        <p>
+          With enough training anything is possible. Here I am stretching before
+          an upcomming karate tournament. I won first place with a deadly
+          scissor kick. My legs are now registered as deadly weapons.
+        </p>
+        <Image
+          alt="Awful retro gif that says resume."
+          src="/logie.png"
+          width={900}
+          height={600}
+          layout="responsive"
+        />
+        <p>
+          This is my 10th Logie win for oustanding performance on the hit show
+          "Secret Life of Us".
+        </p>
       </div>
-      <div className={center}>
-        <div className="window">
-          <div className="title-bar">
-            <div className="title-bar-text">Command Prompt</div>
-            <div className="title-bar-controls">
-              <button aria-label="Minimize"></button>
-              <button aria-label="Maximize"></button>
-              <button aria-label="Close"></button>
-            </div>
-          </div>
-          <div className="window-body">
-            <pre>
-              Welcome To GreyDOS 1.0 &#10094;C&#10095;
-              {/* <br />C:&#92;WINDOWS&#92;SYSTEM32&gt; Welcome to my home page. */}
-              <br />C:&#92;dir
-              <br /> Volume in drive C is OS
-              <br /> Volume Serial Number is 1337-C0D3R
-              <br />
-              <br /> Directory of C:&#92;
-              <br />
-              <br />2021-01-01 12:01PM  &#10094;DIR&#10095;  <Link href="/"><a>HOME</a></Link>
-              <br />    6 Dir(s) 34,871,164 bytes free
-              <br />C:&#92;
-              {/* <br />C:&#92;GreyDOS&#92;RESUME&gt; Resume
-              <br />C:&#92;GreyDOS&#92;ABOUT&gt; About Me
-              <br />C:&#92;GreyDOS&#92;GITHUB&gt; GitHub
-              <br />C:&#92;GreyDOS&#92;TWITTER&gt; Twitter
-              <br />C:&#92;GreyDOS&#92;LINKEDIN&gt; Linkedin
-              <br />C:&#92;GreyDOS&#92;PROJECTS&gt; Projects */}
-            </pre>
-          </div>
-        </div>
-        </div>
     </div>
-  )
+  );
 }
